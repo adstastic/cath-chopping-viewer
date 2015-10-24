3f7386eaf6901761aa78aff66d5cf58454a3dfe0
Adi Mukherjee <ads.mukherjee@gmail.com>
2015-10-24 16:42:32 +0100

    sync changes

--- app/domchop.js
+++ app/domchop.js
@@ -856,16 +856,7 @@ var app = new Marionette.Application.extend({
   }
 });
 
-app.on('start', function() {
-  Backbone.history.start();
-});
-
 var queryString = 'colouring=chopping&id=1fup&chopping=1fup%20D79-109%5BB%5D%2B225-393%5BB%5D%20D110-224%5BB%5D%20D410-547%5BB%5D%20F43-78%5BB%5D%20F394-409%5BB%5D';
-
-var app = new app({
-  container: '#cv-container',
-  query: queryString
-});
 
 var pdbViewer = new app.App({
   query: queryString
